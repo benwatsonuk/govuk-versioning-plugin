6267448117890b4e959c3c75eb9f76efe9ee9fa0
benwatsonuk <hello@benwatson.eu>
2026-04-29 20:03:38 +0100

    Started to put the thing together

---
+++ scripts/bump-version.js
@@ -0,0 +1,13 @@
+const { bumpVersion } = require("../lib/version-bumper");
+
+const [appName, fromVersion, toVersion] = process.argv.slice(2);
+
+if (!appName || !fromVersion || !toVersion) {
+  console.log(
+    "Usage: npm run bump-version"
+  );
+  process.exit(1);
+}
+
+bumpVersion(appName, fromVersion, toVersion)
+  .catch(console.error);
